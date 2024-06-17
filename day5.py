bd6e2b163d96f95809caa2bd563d204a25872bca
madhura <madhura.pandimane@gmail.com>
2024-06-17 18:33:05 +0530

    first commit.

---
+++ day5.py
@@ -0,0 +1,16 @@
+n = [11,22,33,44,121,34,21]
+
+for n1 in n:  #n1 is considered the number of iterations done for the FOR LOOP
+    
+    print(n1)
+
+n.insert(0,'a')
+print(n)
+
+n.append('d')
+print(n)
+
+print(n[5])
+
+n.pop(3)
+print(n)
