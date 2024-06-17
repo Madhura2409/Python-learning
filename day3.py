0ad4b9fe8626dd35c46f4554ae0e66d24e7c8c49
madhura <madhura.pandimane@gmail.com>
2024-06-17 18:55:26 +0530

    Remove 0's from the list

---
+++ day3.py
@@ -0,0 +1,9 @@
+#Even Nos & Odd nos
+
+n = [1,2,3,4,5,6]
+for n1 in n:
+    if n1 % 2 == 0:
+        print(n1,'Even no')
+    else:
+        print(n1,'Odd no')
+    
---
+++ day5(1).py
@@ -0,0 +1,8 @@
+#remove the 0's from the list
+n=[8,9,3,1,2,0,5,0] 
+for n1 in n: 
+
+    if n1 != 0:
+     print(n1)
+
+    
